fe9143fbfca9af46cab742d719339fba749aefe5
Kevin <kevinp.codes@gmail.com>
2022-07-13 10:30:37 -0400

    update - storing teams

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ num_teams = input("Enter the number of teams in the tournament: ")
 # team naming -- need to store the team names in some way
 for num in range(int(num_teams)):
     num = 1
-    
+    team_names = {}
+
     while num <= int(num_teams):
-        print(input(f"Enter the name for team #{num}: "))
+        new_team = input(f"Enter the name for team #{num}: ")
+        team_names[f"team #{num}"] = new_team
         num +=1
     break
 
-    
+# number of games played by each team
+num_games = input("Enter the number of games played by each team: ")
+
+# number of wins each team had
