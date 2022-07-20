1e02e699cafa5d077b405bc412c2e1ce0fa19161
Kevin <kevinp.codes@gmail.com>
2022-07-20 16:07:41 -0400

    Add user input validation on num_of_team()

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# this will store a value for number of teams in tournament
-num_teams = input("Enter the number of teams in the tournament: ")
+# this will house the first operation -- taking in the number of teams
+def num_of_team():
+    global num_teams 
+    num_teams = input("Enter the number of teams in the tournament: ")  # this will store a value for number of teams in tournament
+    if int(num_teams) < 2:  # this will validate the user input
+        print("The minimum number of teams is 2, try again.")
+        return num_of_team()
 
-# team naming and storing
-for num in range(int(num_teams)):
-    num = 1
-    team_names = {}
+# this will house the second operation -- team naming and storing // LEFT OFF: ADD USER INPUT VALIDATION 2nd OP
+def name_teams():
+    for num in range(int(num_teams)):
+        num = 1
+        team_names = {}
 
-    while num <= int(num_teams):
-        new_team = input(f"Enter the name for team #{num}: ")
-        team_names[f"team #{num}"] = new_team
-        num +=1
-    break
+        while num <= int(num_teams):
+            new_team = input(f"Enter the name for team #{num}: ")
+            team_names[f"team #{num}"] = new_team
+            num +=1
+        break
 
 # number of games played by each team
 num_games = input("Enter the number of games played by each team: ")
